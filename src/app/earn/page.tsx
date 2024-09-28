e207fb1c1cf42934bdf2c8f9ebdf97e22416cc4f
Toprak Henaz <toprakhenaz@gmail.com>
2024-09-28 18:13:32 +0300

    .

--- src/app/earn/page.tsx
+++ src/app/earn/page.tsx
@@ -1,10 +1,47 @@
-import { UserProvider } from '../context/UserContext';
-import Earn from './EarnPage';
+'use client';
+import { useState, useEffect} from 'react';
+import axios from 'axios';
+import Earn from '@/components/Earn/Earn';
+import { Mission, User } from '@prisma/client';
+import SkeletonLoading from '../skeleton/SkeletonEarn';
+import { useUserContext } from '../context/UserContext';
 
-export default function MinePage() {
-  return (
-    <UserProvider>
-      <Earn />
-    </UserProvider>
-  );
+
+interface UserWithMissions extends User {
+  missions: Mission[];
+}
+
+export default function EarnPage() {
+  const [user, setUser] = useState<UserWithMissions | null>(null);
+  const { userId, setUserId } = useUserContext();
+
+
+  const fetchUserData = async (id: number) => {
+    try {
+      const response = await axios.post('/api/fetch-earn', { userId: id });
+      setUser(response.data);
+    } catch (error) {
+      console.error('Error fetching user data:', error);
+    }
+  };
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+        setUserId(userId);
+        fetchUserData(userId);
+      } else {
+        fetchUserData(1);
+      }
+
+  }, []);
+
+
+
+  // Kullanıcı verisi yüklenmemişse veya boşsa gösterilecek durum
+  if (!user) {
+    return <SkeletonLoading />;
+  }
+
+
+  return <Earn user={user} />;
 }
