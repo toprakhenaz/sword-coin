f73a31a4a7c9d03a74a65e465afdf53cc6504da7
Toprak Henaz <toprakhenaz@gmail.com>
2024-10-02 23:13:21 +0300

    .

--- src/app/api/fetch-user/route.ts
+++ src/app/api/fetch-user/route.ts
@@ -2,82 +2,74 @@ import { NextRequest, NextResponse } from 'next/server';
 import prisma from "@/db";
 
 export async function POST(request: NextRequest) {
-  let errorMessage = '';
-  try {
+
     const { TelegramUser, startParam } = await request.json();
     console.log('Incoming Telegram User:', TelegramUser); // Gelen Telegram kullanıcısını logla
 
     const userId = TelegramUser ? TelegramUser.id : 1;
-    console.log('User ID determined as:', userId); 
-    console.log('request:', request);
-    let user ;
-    try {
-      user = await prisma.user.findFirst({
-        where: { id: userId },
-      });
-    }
-    catch (error){ 
-      errorMessage = 'Usere girmedi';
-      console.log(error);
-      if (!user) {
-        console.log('User not found, creating new user with ID:', userId); 
-        let coin = 0;
-        errorMessage = 'usere girdi';
-        if (startParam) {
-          errorMessage = ' start parama girdi';
-          const referrer = await prisma.user.findFirst({
-            where: { id: parseInt(startParam) },
-          });
+    console.log('User ID determined as:', userId);
 
-          if (referrer) {
-            errorMessage =' referrere girdi ';
-            console.log('Referrer found with ID:', referrer.id); 
-            await prisma.referance.create({
-              data: {
-                referenceId: referrer.id,
-                referencedId: userId,
-                referancedName: TelegramUser?.username || "Anonymous",
-                referenceAmount: 2500,
-                isClaimed: false,
-                previousLig: 1,
-              },
-            });
-          } else {
-            return NextResponse.json({ message: 'Referrer not found', success: false }, { status: 404 });
-          }
+    // Kullanıcıyı veritabanında arıyoruz
+    let user = await prisma.user.findFirst({
+      where: { id: userId },
+    });
 
-          coin = 2500;
-        }
+    // Eğer kullanıcı yoksa, yeni kullanıcı oluştur
+    if (!user) {
+      console.log('User not found, creating new user with ID:', userId);
+      let coin = 0;
 
-        user = await prisma.user.create({
-          data: {
-            id: userId,
-            userName: TelegramUser?.username || "Anonymous",
-            userImage: null,
-            coins: coin,
-            energy: 500,
-            energyMax: 500,
-            league: 1,
-            coinsHourly: 0,
-            coinsPerTap: 1,
-            lastBoostTime: new Date('2023-01-01T00:00:00Z'),
-            dailyBoostCount: 3,
-            dailyCardRewardClaimed: false,
-            foundCards: "",
-            dailyRewardDate: new Date('2023-01-01T00:00:00Z'),
-            dailyRewardStreak: 1,
-            dailyRewardClaimed: false,
-          },
+      // Eğer startParam varsa, referans kontrolü yapılır
+      if (startParam) {
+        const referrer = await prisma.user.findFirst({
+          where: { id: parseInt(startParam) },
         });
-        console.log('New user created with ID:', userId);
+
+        if (referrer) {
+          console.log('Referrer found with ID:', referrer.id);
+          await prisma.referance.create({
+            data: {
+              referenceId: referrer.id,
+              referencedId: userId,
+              referancedName: TelegramUser?.username || "Anonymous",
+              referenceAmount: 2500,
+              isClaimed: false,
+              previousLig: 1,
+            },
+          });
+          coin = 2500;
+        } else {
+          return NextResponse.json({ message: 'Referrer not found', success: false }, { status: 404 });
+        }
       }
-      return NextResponse.json({ user, success: true });
+
+      // Yeni kullanıcı oluştur
+      user = await prisma.user.create({
+        data: {
+          id: userId,
+          userName: TelegramUser?.username || "Anonymous",
+          userImage: null,
+          coins: coin,
+          energy: 500,
+          energyMax: 500,
+          league: 1,
+          coinsHourly: 0,
+          coinsPerTap: 1,
+          lastBoostTime: new Date('2023-01-01T00:00:00Z'),
+          dailyBoostCount: 3,
+          dailyCardRewardClaimed: false,
+          foundCards: "",
+          dailyRewardDate: new Date('2023-01-01T00:00:00Z'),
+          dailyRewardStreak: 1,
+          dailyRewardClaimed: false,
+        },
+      });
+      console.log('New user created with ID:', userId);
     }
+
+    // Başarılı işlem sonucunda kullanıcıyı döndür
     return NextResponse.json({ user, success: true });
-  } catch (error) {
-    console.error('Error during user creation or fetching:', error);
-    return NextResponse.json({ message: `${errorMessage}`, success: false }, { status: 500 });
-  }
+
 }
 
 export async function GET() {
