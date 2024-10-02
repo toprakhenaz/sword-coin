70eac7338ff5e807ecaa3c32588988cf0a61d8b5
Toprak Henaz <toprakhenaz@gmail.com>
2024-10-02 22:58:21 +0300

    .

--- src/app/api/fetch-user/route.ts
+++ src/app/api/fetch-user/route.ts
@@ -10,64 +10,67 @@ export async function POST(request: NextRequest) {
     const userId = TelegramUser ? TelegramUser.id : 1;
     console.log('User ID determined as:', userId); 
     console.log('request:', request);
-
-    let user = await prisma.user.findFirst({
-      where: { id: userId },
-    });
-    errorMessage = 'Usere girmedi';
-    if (!user) {
-      console.log('User not found, creating new user with ID:', userId); 
-      let coin = 0;
-      errorMessage = 'usere girdi';
-      if (startParam) {
-        errorMessage = errorMessage + ' start parama girdi';
-        const referrer = await prisma.user.findFirst({
-          where: { id: parseInt(startParam) },
-        });
-
-        if (referrer) {
-          errorMessage = errorMessage + ' referrere girdi ';
-          console.log('Referrer found with ID:', referrer.id); 
-          await prisma.referance.create({
-            data: {
-              referenceId: referrer.id,
-              referencedId: userId,
-              referancedName: TelegramUser?.username || "Anonymous",
-              referenceAmount: 2500,
-              isClaimed: false,
-              previousLig: 1,
-            },
+    let user ;
+    try {
+      user = await prisma.user.findFirst({
+        where: { id: userId },
+      });
+    }
+    catch{ 
+      errorMessage = 'Usere girmedi';
+      if (!user) {
+        console.log('User not found, creating new user with ID:', userId); 
+        let coin = 0;
+        errorMessage = 'usere girdi';
+        if (startParam) {
+          errorMessage = errorMessage + ' start parama girdi';
+          const referrer = await prisma.user.findFirst({
+            where: { id: parseInt(startParam) },
           });
-        } else {
-          return NextResponse.json({ message: 'Referrer not found', success: false }, { status: 404 });
+
+          if (referrer) {
+            errorMessage = errorMessage + ' referrere girdi ';
+            console.log('Referrer found with ID:', referrer.id); 
+            await prisma.referance.create({
+              data: {
+                referenceId: referrer.id,
+                referencedId: userId,
+                referancedName: TelegramUser?.username || "Anonymous",
+                referenceAmount: 2500,
+                isClaimed: false,
+                previousLig: 1,
+              },
+            });
+          } else {
+            return NextResponse.json({ message: 'Referrer not found', success: false }, { status: 404 });
+          }
+
+          coin = 2500;
         }
 
-        coin = 2500;
+        user = await prisma.user.create({
+          data: {
+            id: userId,
+            userName: TelegramUser?.username || "Anonymous",
+            userImage: null,
+            coins: coin,
+            energy: 500,
+            energyMax: 500,
+            league: 1,
+            coinsHourly: 0,
+            coinsPerTap: 1,
+            lastBoostTime: new Date('2023-01-01T00:00:00Z'),
+            dailyBoostCount: 3,
+            dailyCardRewardClaimed: false,
+            foundCards: "",
+            dailyRewardDate: new Date('2023-01-01T00:00:00Z'),
+            dailyRewardStreak: 1,
+            dailyRewardClaimed: false,
+          },
+        });
+        console.log('New user created with ID:', userId);
       }
-
-      user = await prisma.user.create({
-        data: {
-          id: userId,
-          userName: TelegramUser?.username || "Anonymous",
-          userImage: null,
-          coins: coin,
-          energy: 500,
-          energyMax: 500,
-          league: 1,
-          coinsHourly: 0,
-          coinsPerTap: 1,
-          lastBoostTime: new Date('2023-01-01T00:00:00Z'),
-          dailyBoostCount: 3,
-          dailyCardRewardClaimed: false,
-          foundCards: "",
-          dailyRewardDate: new Date('2023-01-01T00:00:00Z'),
-          dailyRewardStreak: 1,
-          dailyRewardClaimed: false,
-        },
-      });
-      console.log('New user created with ID:', userId);
     }
-
     return NextResponse.json({ user, success: true });
   } catch (error) {
     console.error('Error during user creation or fetching:', error);
